69ffbf1c61999cfe6a36de4238586ab33c1467c8
Aba Wandjovu Nicaisse <abawandjovunicaise@gmail.com>
2022-02-07 19:15:10 +0300

    Created the modal Window for booking an appointment

--- index.js
+++ index.js
@@ -16,3 +16,47 @@ function toggleNavMenu() {
         navMenu.style.animation = "menu-slide-out 1s ease-out";
     }
 }
+
+//Top Up the Booking Modal Window to Book an Appointment//
+    
+// Modal Window
+const modalWindow = document.getElementById("book-modal");
+
+const popUpBtns = [];
+
+// Add "Contact Us" btn to the buttons that popup the modal 
+popUpBtns.push(document.getElementById("contact-us"));
+
+bookAppointmentBtns = Array.from(document.getElementsByClassName("book-appointment"));
+// Add all "book an appointment" btn to popup the Modal 
+bookAppointmentBtns.forEach(btn => popUpBtns.push(btn))
+
+popUpBtns.forEach(btn => btn.addEventListener("click", popUpModal))
+
+function popUpModal() {
+    modalWindow.style.display = "block"
+}
+
+// close modal window
+const closeModalBtns = [];
+
+closeModalBtns.push(document.getElementById("close-modal"), document.getElementById("modal-submit"));
+
+closeModalBtns.forEach(btn => btn.addEventListener("click", closeModal))
+
+function closeModal(e) {
+    e.preventDefault()
+    modalWindow.style.display = "none"
+    
+    if (e.target == modalWindow) {
+        modalWindow.style.display = "none"
+    }
+}
+
+// Also close on click outside the modal Window
+window.addEventListener("click", closeModalOnWindowClick);
+function closeModalOnWindowClick(e) {
+    if (e.target == modalWindow) {
+        modalWindow.style.display = "none"
+    }
+}
